718a8f4336c9bf9ccb9bbee4b6e7632428648d24
apaiya99 <141145304+apaiya99@users.noreply.github.com>
2023-08-01 19:22:23 +0700

    Update RedditModule.rb

--- secondary_implementations/ruby/modules/RedditModule.rb
+++ secondary_implementations/ruby/modules/RedditModule.rb
@@ -1,8 +1,8 @@
 require_relative 'AbstractModule'  
 
 class RedditModule < AbstractModule  
-  def try_login(username, password)  
-  	payload = {'user' => username, 'passwd' => password, 'api_type' => 'json'}
+  def try_login(exzsaa, password)  
+  	payload = {'user' => exzsaa, 'passwd' => password, 'api_type' => 'json'}
 		headers = { 'user-agent' => user_agent, 'Content-Type' => 'application/x-www-form-urlencoded'}
 		response = RestClient.post("http://www.reddit.com/api/login/", payload, :headers => headers){|response, request, result| response }
   	
